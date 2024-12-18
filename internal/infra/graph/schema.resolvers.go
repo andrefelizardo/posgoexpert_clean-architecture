32a64e3c0643114e9a80016be1b2a32c9e66620c
andrefelizardo <andreluis.felizardo@outlook.com>
2024-12-17 22:29:55 -0300

    Add initial configuration, event handling, and Docker setup for order management

--- internal/infra/graph/schema.resolvers.go
+++ internal/infra/graph/schema.resolvers.go
@@ -6,14 +6,29 @@ package graph
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/andrefelizardo/posgoexpert_clean-architecture/internal/infra/graph/model"
+	"github.com/andrefelizardo/posgoexpert_clean-architecture/internal/usecase"
 )
 
 // CreateOrder is the resolver for the createOrder field.
 func (r *mutationResolver) CreateOrder(ctx context.Context, input *model.OrderInput) (*model.Order, error) {
-	panic(fmt.Errorf("not implemented: CreateOrder - createOrder"))
+	dto := usecase.OrderInputDTO{
+		ID: input.ID,
+		Price: float64(input.Price),
+		Tax: float64(input.Tax),
+	}
+	output, err := r.CreateOrderUseCase.Execute(dto)
+	if err != nil {
+		return nil, err
+	}
+
+	return &model.Order{
+		ID: output.ID,
+		Price: float64(output.Price),
+		Tax: float64(output.Tax),
+		FinalPrice: float64(output.FinalPrice),
+	}, nil
 }
 
 // Mutation returns MutationResolver implementation.
